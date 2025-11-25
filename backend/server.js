8f9e1c55d4bb7182b02d17b94abf4cbed404f36f
Richa <reikhandekar05@gmail.com>
2025-11-25 10:35:24 +0530

    Day 1: Initial setup + backend server running

---
+++ backend/server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/",(req,res)=>{
+    res.send("Task manager backend started");
+});
+
+app.listen(5000,()=>{
+    console.log("Server started on port 5000");
+});
