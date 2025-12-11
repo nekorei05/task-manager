5cf2c62e6c9a1229bafa006775aea5a9dde47c00
Richa <reikhandekar05@gmail.com>
2025-12-11 23:20:53 +0530

    Added CORS for fetching

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 require("dotenv").config();
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.get("/",(req,res)=>{
     res.send("Task manager backend started");
