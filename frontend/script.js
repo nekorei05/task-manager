2675595275db43596f274e45f995ed3339fb0cee
Richa <reikhandekar05@gmail.com>
2025-12-23 21:58:42 +0530

    Updated functions for add,delete

--- frontend/script.js
+++ frontend/script.js
@@ -8,51 +8,66 @@ async function getTasks(){
 }
 
 function displayTasks(tasks){
-    const list = document.getElementById("taskList");
-    list.innerHTML = "";
+    const pendinglist = document.getElementById("pending-list");
+    const completedlist = document.getElementById("completed-list");
+
+    pendinglist.innerHTML = "";
+    completedlist.innerHTML = "";
+
 
     tasks.forEach(task => {
         const div = document.createElement("div");
         div.className = "task";
-
-        div.innerHTML = `
-        <span>${task.title}(${task.status})</span>
-        <button onclick="deleteTask('${task._id}')">Delete</button>
+        div.draggable = true;
+        div.dataset.id = task._id;
+                div.innerHTML = `
+        <span>${task.title}</span>
+        <button class="edit"> 0 </button>
         `;
-
-        list.appendChild(div);
+        if (task.status==="completed"){
+            completedlist.appendChild(div);
+        } else {
+            pendinglist.appendChild(div);
+        }
     });
 }
-async function deleteTask(id) {
-    await fetch(`${baseUrl}/${id}`, {
-        method: "DELETE"
-    });
-    getTasks(); // refresh list
-}
 
-getTasks();
-
+//Add task
 document.getElementById("addBtn").addEventListener("click",addTask);
 async function addTask(){
-    const title = document.getElementById("taskTitle").value;
+    const input = document.getElementById("taskTitle");
+    const title = input.value.trim();
 
-    if(title.trim()===""){
-        alert(" Task cannot be empty");
+    if(!title){
+        alert("Task cannot be empty");
         return;
     }
 
     await fetch(baseUrl, {
         method : "POST",
-        headers : {
-            "Content-type" : "application/json"
-        },
-        body : JSON.stringify({
-            title : title,
-            status : "pending"
-        })
+        headers: { "Content-Type": "application/json"},
+        body : JSON.stringify({ title, status : "pending"})
     });
 
-    document.getElementById("taskTitle").value="";
+    input.value = "";
     getTasks();
-
 }
+
+let draggedTask = null;
+document.addEventListener("dragstart", e => {
+    if(e.target.classList.contains("task")){
+        draggedTask = e.target;
+    }
+});
+
+
+
+//delete task
+const trash = document.getElementById("trash");
+trash.addEventListener("dragover", e=> e.preventDefault());
+trash.addEventListener("drop", async()=> {
+    const taskId = draggedTask.dataset.id;
+    await fetch(`${baseUrl}/${taskId}`, { method : "DELETE"});
+    draggedTask.remove();
+});
+
