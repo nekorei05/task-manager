5d674cc8d821682ab9a03708c693248a093a6471
Richa <reikhandekar05@gmail.com>
2025-12-24 18:47:56 +0530

    Added drag

--- frontend/script.js
+++ frontend/script.js
@@ -1,73 +1,53 @@
+console.log("JS is connected")
+document.addEventListener("click", (e)=>{
+    console.log("CLicked");
+    console.log(e);
+});
 
-const baseUrl = "http://localhost:5000/tasks";
+document.addEventListener("click", (e)=> {
+    if(e.target.id=== "addBtn")
+    console.log("You clicked on add");
+    
+});
 
-async function getTasks(){
-    const res = await fetch(baseUrl);
-    const tasks = await res.json();
-    displayTasks(tasks);
-}
+const pendingList = document.getElementById("pending-list");
+document.getElementById("addBtn").addEventListener("click", e=> {
+    const input = document.getElementById("taskInput");
+    const title = input.value;
 
-function displayTasks(tasks){
-    const pendinglist = document.getElementById("pending-list");
-    const completedlist = document.getElementById("completed-list");
-
-    pendinglist.innerHTML = "";
-    completedlist.innerHTML = "";
-
-
-    tasks.forEach(task => {
-        const div = document.createElement("div");
-        div.className = "task";
-        div.draggable = true;
-        div.dataset.id = task._id;
-                div.innerHTML = `
-        <span>${task.title}</span>
-        <button class="edit"> 0 </button>
-        `;
-        if (task.status==="completed"){
-            completedlist.appendChild(div);
-        } else {
-            pendinglist.appendChild(div);
-        }
-    });
-}
-
-//Add task
-document.getElementById("addBtn").addEventListener("click",addTask);
-async function addTask(){
-    const input = document.getElementById("taskTitle");
-    const title = input.value.trim();
-
-    if(!title){
-        alert("Task cannot be empty");
+    if(title.trim()===""){
+        alert("task cannot be empty");
         return;
     }
 
-    await fetch(baseUrl, {
-        method : "POST",
-        headers: { "Content-Type": "application/json"},
-        body : JSON.stringify({ title, status : "pending"})
-    });
+    const taskDiv = document.createElement("div");
+    taskDiv.className = "task";
+    taskDiv.textContent = title;
+    taskDiv.draggable = true;
 
+    pendingList.appendChild(taskDiv);
     input.value = "";
-    getTasks();
-}
+});
 
 let draggedTask = null;
-document.addEventListener("dragstart", e => {
+document.addEventListener("dragstart", e=>{
+    console.log("Drag started on : ",e.target);
+
     if(e.target.classList.contains("task")){
         draggedTask = e.target;
     }
 });
 
-
-
-//delete task
-const trash = document.getElementById("trash");
-trash.addEventListener("dragover", e=> e.preventDefault());
-trash.addEventListener("drop", async()=> {
-    const taskId = draggedTask.dataset.id;
-    await fetch(`${baseUrl}/${taskId}`, { method : "DELETE"});
-    draggedTask.remove();
+const completedList = document.getElementById("completed-list");
+completedList.addEventListener("dragover",e=>{
+    e.preventDefault();
 });
 
+completedList.addEventListener("drop",e=>{
+    e.preventDefault();
+
+    if(draggedTask){
+    completedList.appendChild(draggedTask);
+    draggedTask = null;
+    }
+});
